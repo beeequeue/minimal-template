ec06265a2aff6012d1541a95b50745f0c7925d06
Adam <adam@haglund.dev>
2022-11-11 22:33:03 +0100

    add eslint-define-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
-module.exports = {
+// @ts-check
+const { defineConfig } = require("eslint-define-config")
+
+module.exports = defineConfig({
   root: true,
+  ignorePatterns: ["*eslintrc*"],
   extends: ["plugin:@beequeue/base", "plugin:@beequeue/typescript"],
   rules: {
     "@typescript-eslint/no-non-null-assertion": "off",
   },
-}
+})
