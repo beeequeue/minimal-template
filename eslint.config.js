bfd9f44f58df34170ef907efa9a860f36e2dafc5
Adam <adam@haglund.dev>
2024-11-30 23:33:35 +0100

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import antfu from "@antfu/eslint-config"
 
 export default antfu({
+  ignores: ["**/*.json"],
   stylistic: false,
   test: { overrides: { "test/no-import-node-test": "off" } },
   typescript: {
@@ -12,11 +13,14 @@ export default antfu({
       "ts/consistent-type-imports": ["error", { fixStyle: "inline-type-imports" }],
       "ts/no-unsafe-argument": "off",
       "ts/no-unsafe-assignment": "off",
+      "node/prefer-global/process": "off",
+      "antfu/no-top-level-await": "off",
+
       "perfectionist/sort-imports": [
         "error",
         {
           type: "natural",
-          internalPattern: ["@/**", "~/**"],
+          internalPattern: ["@/.+?", "~/.+?"],
           newlinesBetween: "always",
           groups: [
             ["builtin", "builtin-type"],
