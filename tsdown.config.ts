7a213c8a30d5d0226ac88f863842cd54888625dc
Adam <adam@haglund.dev>
2025-04-22 18:31:00 +0200

    use tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,28 @@
+import process from "node:process"
+
+import { defineConfig } from "tsdown"
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  outDir: "dist",
+  clean: true,
+
+  define: {
+    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV ?? "production"),
+    "process.env.DEV": JSON.stringify(process.env.NODE_ENV === "development"),
+    "process.env.PROD": JSON.stringify(process.env.NODE_ENV === "production"),
+    "process.env.TEST": JSON.stringify(false),
+  },
+
+  platform: "node",
+  target: "node22",
+  format: "esm",
+  dts: true,
+  fixedExtension: true,
+
+  minify: true,
+  sourcemap: true,
+  outputOptions: (op) => {
+    // op.polyfillRequire = true
+  },
+})
