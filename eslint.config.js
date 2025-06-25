3c34c3c34b4f07aeb593866121f5a741ce9266e6
Adam <adam@haglund.dev>
2025-06-26 00:32:32 +0200

    fix bad rule

--- eslint.config.js
+++ eslint.config.js
@@ -50,6 +50,7 @@ export default antfu({
       "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
       "node/prefer-global/process": "off",
       "ts/consistent-type-definitions": "off",
+      "ts/consistent-type-imports": ["error", { fixStyle: "inline-type-imports" }],
       "ts/no-unsafe-argument": "off",
       "ts/no-unsafe-assignment": "off",
       "ts/no-use-before-define": "off",
