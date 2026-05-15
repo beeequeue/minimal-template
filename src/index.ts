09c44be9807b66600a3b932b3799670139a891d0
bq <adam@haglund.dev>
2026-05-15 14:32:22 +0200

    up deps

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import pkgJson from "@beequeue/project-template/package.json" with { type: "json" }
+
 export const hello = (): string => {
-	console.log("hello world")
+	console.log("hello world:", pkgJson.version)
 	return "world"
 }
